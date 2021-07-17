01a5bc5e2382efd9ea2d0790d224aaa2a6b5e694
LukaErnestini <luka.ernestini@gmail.com>
2021-07-17 18:13:08 +0200

    added example css and view

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
+
+// Define paths for express config
+const publicDirPath = path.join(__dirname, '../public');
+
+// Setup static directory to serve
+app.use(express.static(publicDirPath));
+
+// view engine configuration
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '../templates/views'));
+
+app.get('/', (req, res) => {
+  res.render('todo.ejs');
+});
+
+app.listen(3000, () => console.log('Server Up and running'));
