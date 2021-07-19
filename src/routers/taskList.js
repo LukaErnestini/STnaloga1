25cd4297ae9a8373eb15556b503dbeada0e11c35
LukaErnestini <luka.ernestini@gmail.com>
2021-07-19 17:52:01 +0200

    added taskList routes

--- src/routers/taskList.js
+++ src/routers/taskList.js
@@ -2,4 +2,27 @@ const express = require('express');
 const router = new express.Router();
 const TaskList = require('../models/taskList');
 
+// Get all tasks
+router.get('/tasklist', async (req, res) => {
+  const tasklist = await TaskList.find({});
+
+  try {
+    //   res.render('tasks', { tasks, title: 'Tasks' });
+    res.send({ taskslist });
+  } catch (e) {
+    res.status(500).send(e);
+  }
+});
+
+// Submit task
+router.post('/tasklist', async (req, res) => {
+  try {
+    const tasklist = new TaskList(req.body);
+    await tasklist.save();
+    res.redirect('/');
+  } catch (e) {
+    res.status(400).send();
+  }
+});
+
 module.exports = router;
